58e0eb9dfcda35ca2ef5e9329e23a2d7728f6b12
Y4trms <99075925@mydavinci.nl>
2024-02-08 09:44:12 +0100

    s

--- lightswitch.py
+++ lightswitch.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 window = tk.Tk()
 
-button = tk.Button(text='...', bg="white", fg="black")
+button = tk.Button(text='lights on', bg="white", fg="black")
 button.pack(pady = 20, padx = 20)
 
 # schijf hier tussen je code
